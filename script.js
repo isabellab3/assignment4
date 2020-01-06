071c9d027fa5c416d3217f07c199a3515ee00ac7
Brittany Cho <bac411@nyu.edu>
2020-01-06 17:05:25 -0500

    part4

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ window.onload=function() {
   text.addEventListener("mouseover", handleHover, false)     
 }
 
+
+/* 
+
+Part 4 
+
+In a Javascript file, write a program which checks the following things:
+-checks that the password is 12345678
+-if the password is incorrect, send an alert message
+-Your page should also include an <h1> tag. If the information in the form is correct, have Javascript change the text in the <h1>.HTML page.
+
+
+*/
+
+function checkPwd() {
+  let pwd = document.getElementById("password").value;
+  let textChange = document.getElementById("textChange");
+
+  if(pwd != 12345678) {
+    alert('Wrong password!')
+  }
+  else {
+    textChange.innerHTML = "The information is correct!"
+  }
+}
+
